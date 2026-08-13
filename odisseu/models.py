e4fa461f41981e3bbf62acf9864549e107ed537b
camily <camilyzipora08@gmail.com>
2026-08-13 15:22:55 -0300

    model paciente

--- odisseu/models.py
+++ odisseu/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Paciente(models.Model):
+    nome = models.CharField(max_length=200)
+    cpf = models.CharField(max_length=11)
+    cns = models.CharField(max_length=14)
+    data_nascimento = models.DateField()
+    telefone = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.nome
--- odisseu/views.py
+++ odisseu/views.py
@@ -1,5 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import PacienteForm
+from .models import Paciente
 
 def index(request):
     return render(request, "index.html")
+
+def pacientes(request):
+    pacientes= Paciente.objects.all()
+
+    return render(request, 'pacientes.html', {
+        'pacientes': pacientes
+    })
+
+
+def cadastrar_paciente(request):
+    if request.method == 'POST':
+        form= PacienteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('pacientes')
+    else:
+        form= PacienteForm()
+
+    context = {
+        'form': form, 
+    }
+
+    return render(request, 'cadastrar_paciente.html', context)
 
