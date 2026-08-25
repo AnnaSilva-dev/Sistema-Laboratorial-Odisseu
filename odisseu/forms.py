12d7f56d4b78da0bfd734733ebaaa760355bfa48
Paula-Dias17 <m.paula3405@gmail.com>
2026-08-25 19:23:42 -0300

    erros nos forms, detalhes

--- odisseu/forms.py
+++ odisseu/forms.py
@@ -5,6 +5,7 @@ from .validators import validate_future_date
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from datetime import date
+import re
 
 
 class UsuarioForm(UserCreationForm):
@@ -12,11 +13,15 @@ class UsuarioForm(UserCreationForm):
     perfil = forms.ChoiceField(
         label='Perfil',
         choices=[
+            ('', 'Selecione seu perfil'),
             ('Administrador', 'Administrador'),
             ('Farmaceutico', 'Farmacêutico'),
             ('Recepcionista', 'Recepcionista'),
             ('Tecnicolaboratorial', 'Técnico Laboratorial'),
-        ]
+        ],
+        widget=forms.Select(attrs={
+            'class': 'perfil edit-forms',
+        })
     )
 
     class Meta:
@@ -25,8 +30,49 @@ class UsuarioForm(UserCreationForm):
             'username',
             'first_name',
             'last_name',
-            'email',
+            'email',        
         ]
+
+        widgets = {
+            'username': forms.TextInput(attrs={
+                'class': 'nome edit-forms',
+                'placeholder': 'ex.: Souzinha'
+            }),
+
+            'first_name': forms.TextInput(attrs={
+                'class': 'nome edit-forms',
+                'placeholder': 'Ex.: Rubens'
+            }),
+
+            'last_name': forms.TextInput(attrs={
+                'class': 'nome edit-forms',
+                'placeholder': 'Ex.: Magalhães'
+            }),
+
+            'email': forms.EmailInput(attrs={
+                'class': 'email edit-forms',
+                'placeholder': 'Ex.: exemplo@email.com',
+            }),
+
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['username'].help_text = ''
+        self.fields['password1'].help_text = ''
+        self.fields['password2'].help_text = ''
+
+        self.fields['password1'].widget.attrs.update({
+            'class': 'senha edit-forms',
+            'placeholder': 'Digite sua senha',
+        })
+
+        self.fields['password2'].widget.attrs.update({
+            'class': 'senha edit-forms',
+            'placeholder': 'Confirme sua senha',
+        })
+
         
 class PacienteForm(forms.ModelForm):
 
@@ -61,6 +107,14 @@ class PacienteForm(forms.ModelForm):
                 'value': 'aaaa/mm/dd'
             }),
         }
+
+    def clean_cpf(self):
+        cpf = self.cleaned_data['cpf']
+        return re.sub(r'\D', '', cpf)
+
+    def clean_cns(self):
+        cns = self.cleaned_data['cns']
+        return re.sub(r'\D', '', cns)
 
 
 class AgendamentoForm(forms.Form):
